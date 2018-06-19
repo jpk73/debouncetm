eba418a2d231fdbab49f2a21378d777a7556f1b2
jpk73 <32308624+jpk73@users.noreply.github.com>
2018-06-19 20:43:05 +0200

    Update multiple.ino

--- examples/multiple/multiple.ino
+++ examples/multiple/multiple.ino
@@ -22,20 +22,20 @@ unsigned long previousMillis = millis();
 
 void loop() {
   Button1.update();
-  if (Button1.pressed()) Serial.println("Button1 pressed");               // needs reset by released()
-  if (Button1.longpressed(repeat)) Serial.println("Button1 longpressed"); // repeated return of longpress
-  if (Button1.released()) Serial.println("Button1 released");
+  if (Button1.pressed()) Serial.println("Button1 pressed");               // pressed() returns true only if called after released()
+  if (Button1.longpressed(repeat)) Serial.println("Button1 longpressed"); // longpressed(1) repeatedly returns true
+  if (Button1.released()) Serial.println("Button1 released");             // released() returns true only if called after pressed()
 
   Button2.update();
-  if (Button2.pressed()) Serial.println("Button2 pressed");               // needs reset by released()
-  if (Button2.longpressed(once)) Serial.println("Button2 longpressed");   // only one longpress will be detected
+  if (Button2.pressed()) Serial.println("Button2 pressed");
+  if (Button2.longpressed(once)) Serial.println("Button2 longpressed");   // longpressed(0) returns true only once
   if (Button2.released()) Serial.println("Button2 released");
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= 1000) {                           // check every second:
     previousMillis = currentMillis;
-    if (Button1.isHigh()) Serial.println("Button1 HIGH");
-    if (Button1.isLow()) Serial.println("Button1 LOW");
+    if (Button1.isHigh()) Serial.println("Button1 HIGH");                 // isHigh() returns true only if called after released()
+    if (Button1.isLow()) Serial.println("Button1 LOW");                   // isLow() returns true only if called after pressed()
     if (Button2.isHigh()) Serial.println("Button2 HIGH");
     if (Button2.isLow()) Serial.println("Button2 LOW");
   }
