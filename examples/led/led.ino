fd52071d1ddf1ec854551bcc5eef112d9b2cc83d
jpk73 <32308624+jpk73@users.noreply.github.com>
2020-02-23 22:47:47 +0100

    Update led.ino

--- examples/led/led.ino
+++ examples/led/led.ino
@@ -6,9 +6,10 @@
 #define outpin LED_BUILTIN  // led pin
 #define buttonpin        2  // button pin
 #define samplerate    0.75  // poll interval for update() in msec (a safe starting point would be samplerate = 0.1 * bounce-duration in msec)
-#define longpress     0.80  // longpress duration in sec
+#define longpress     0.90  // longpress duration in sec
+#define doubleclick   0.50  // doubleclick window in sec
 
-Button button1(buttonpin, INPUT_PULLUP, LOW, samplerate, longpress);  // specify pin, pinMode, polarity, samplerate, longpress duration
+Button button1(buttonpin, INPUT_PULLUP, LOW, samplerate, longpress, doubleclick);  // specify pin, pinMode, polarity, samplerate, longpress duration, doubleclick window
 
 void setup() {
   pinMode(outpin, OUTPUT);
